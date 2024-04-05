94ed42d161abd051e993caf78c751a682e40f1ab
Vaeri8 <Vaeri8@users.noreply.github.com>
2024-04-06 05:44:25 +0700

    added customer extensions

---
+++ src/Dependent.java
@@ -0,0 +1,8 @@
+//Luong Tuan Kiet - s3980288
+
+class Dependent extends Customer {
+
+    public Dependent(String id, String fullName, String insuranceCard) {
+        super(id, fullName, insuranceCard);
+    }
+}
